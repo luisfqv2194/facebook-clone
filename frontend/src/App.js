bda85cca6e0c004d36e1665b5d835bafb7626efd
Luis Quiros <lquiros@akurey.com>
2022-09-10 07:38:13 -0600

    Applying redux with toolkit most modern approach

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/login'
+import Profile from './pages/profile'
+import Home from './pages/home'
+
 function App() {
-  const get = async () => {
-    const res = await fetch('http://localhost:8000')
-    console.log(res)
-  }
-  get()
-  return <div>welcome to frontend</div>
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  )
 }
 
 export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "./styles/icons/icons.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import './styles/icons/icons.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+)
