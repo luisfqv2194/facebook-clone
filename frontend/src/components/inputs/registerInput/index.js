06cf50a9bf990679fc3915218db1a500c3e7bf2c
Luis Quiros <lquiros@akurey.com>
2022-10-12 20:17:37 -0600

    Fixing CSS and responsiveness

--- frontend/src/components/inputs/registerInput/index.js
+++ frontend/src/components/inputs/registerInput/index.js
@@ -3,52 +3,62 @@ import './style.css'
 import { useMediaQuery } from 'react-responsive'
 export default function RegisterInput({ ...props }) {
   const [field, meta] = useField(props)
-  const desktopView = useMediaQuery({
+  const view1 = useMediaQuery({
+    query: '(min-width: 539px)',
+  })
+  const view2 = useMediaQuery({
     query: '(min-width: 850px)',
   })
+  const view3 = useMediaQuery({
+    query: '(min-width: 1170px)',
+  })
+
+  const isView3AndFirstName = view3 && field.name === 'first_name'
+  const isView3AndLastName = view3 && field.name === 'last_name'
+
   return (
-    <div className='input_wrap'>
-      {meta.touched && meta.error && !props.bottom ? (
-        <div
-          className={desktopView ? 'input_error_desktop' : 'input_error'}
-          style={{ transform: 'translateY(3px)' }}
-        >
-          <ErrorMessage name={field.name} />
-          <div
-            className={desktopView ? 'error_arrow_left' : 'error_arrow_top'}
-          ></div>
-        </div>
-      ) : (
-        ''
-      )}
+    <div className='input_wrap register_input_wrap'>
       <input
         className={meta.touched && meta.error ? 'input_error_border' : ''}
+        style={{
+          width: `${
+            view1 && (field.name === 'first_name' || field.name === 'last_name')
+              ? '100%'
+              : view1 && (field.name === 'email' || field.name === 'password')
+              ? '370px'
+              : '300px'
+          }`,
+        }}
         type={field.type}
         name={field.name}
         {...field}
         {...props}
       ></input>
-      {meta.touched && meta.error && props.bottom ? (
+      {meta.touched && meta.error && (
         <div
-          className={desktopView ? 'input_error_desktop' : 'input_error'}
-          style={{ transform: 'translateY(1px)' }}
+          className={view3 ? 'input_error input_error_desktop' : 'input_error'}
+          style={{
+            transform: 'translateY(2px)',
+            left: `${
+              isView3AndFirstName ? '-107%' : isView3AndLastName ? '107%' : ''
+            }`,
+          }}
         >
-          <ErrorMessage name={field.name} />
-          <div
-            className={desktopView ? 'error_arrow_left' : 'error_arrow_bottom'}
-          ></div>
+          {meta.touched && meta.error && <ErrorMessage name={field.name} />}
+          {meta.touched && meta.error && (
+            <div
+              className={
+                view3 && field.name !== 'last_name'
+                  ? 'error_arrow_left'
+                  : view3 && field.name === 'last_name'
+                  ? 'error_arrow_right'
+                  : !view3 && 'error_arrow_bottom'
+              }
+            ></div>
+          )}
         </div>
-      ) : (
-        ''
       )}
-      {meta.touched && meta.error ? (
-        <i
-          className='error_icon'
-          style={{ top: `${!props.bottom && !desktopView ? '63%' : '15px'}` }}
-        ></i>
-      ) : (
-        ''
-      )}
+      {meta.touched && meta.error ? <i className='error_icon'></i> : ''}
     </div>
   )
 }
--- frontend/src/components/login/RegisterForm.js
+++ frontend/src/components/login/RegisterForm.js
@@ -1,7 +1,7 @@
 import { Form, Formik } from 'formik'
 import { useState } from 'react'
 import RegisterInput from '../inputs/registerInput'
-
+import * as Yup from 'yup'
 const RegisterForm = () => {
   const userInfos = {
     first_name: '',
@@ -36,6 +36,36 @@ const RegisterForm = () => {
     return new Date(bYear, bMonth, 0).getDate()
   }
   const days = Array.from(new Array(getDays()), (val, index) => 1 + index)
+
+  const registerValidationSchema = Yup.object({
+    first_name: Yup.string()
+      .required("What's your First name ?")
+      .min(2, 'Fisrt name must be between 2 and 16 characters.')
+      .max(16, 'Fisrt name must be between 2 and 16 characters.')
+      .matches(
+        /^[aA-zZ\s]+$/,
+        'Numbers and special characters are not allowed.'
+      ),
+    last_name: Yup.string()
+      .required("What's your Last name ?")
+      .min(2, 'Last name must be between 2 and 16 characters.')
+      .max(16, 'Last name must be between 2 and 16 characters.')
+      .matches(
+        /^[aA-zZ\s]+$/,
+        'Numbers and special characters are not allowed.'
+      ),
+    email: Yup.string()
+      .required(
+        "You'll need this when you log in and if you ever need to reset your password."
+      )
+      .email('Enter a valid email address.'),
+    password: Yup.string()
+      .required(
+        'Enter a combination of at least six numbers,letters and punctuation marks(such as ! and &).'
+      )
+      .min(6, 'Password must be atleast 6 characters.')
+      .max(36, "Password can't be more than 36 characters"),
+  })
   console.log(user)
   return (
     <div className='blur'>
@@ -45,7 +75,20 @@ const RegisterForm = () => {
           <span>Sign Up</span>
           <span>It's quick and easy</span>
         </div>
-        <Formik>
+        <Formik
+          enableReinitialize
+          initialValues={{
+            first_name,
+            last_name,
+            email,
+            password,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+          }}
+          validationSchema={registerValidationSchema}
+        >
           {(formik) => (
             <Form className='register_form'>
               <div className='reg_line'>
