574639bf70d52f0e400b7c5414a01f131e72febd
Luis Quiros <lquiros@akurey.com>
2022-10-26 18:10:28 -0600

    Further styling and createPost

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,12 +1,12 @@
 import { useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { ref } from 'yup'
+import CreatePost from '../../components/createPost'
 import Header from '../../components/header'
 import LeftHome from '../../components/home/left'
 import RightHome from '../../components/home/right'
 import Stories from '../../components/home/stories'
 import './style.css'
-const Home = () => {
+export default function Home() {
   const { user } = useSelector((user) => ({ ...user }))
   return (
     <div className='home'>
@@ -14,10 +14,9 @@ const Home = () => {
       <LeftHome user={user} />
       <div className='home_middle'>
         <Stories />
+        <CreatePost user={user} />
       </div>
       <RightHome user={user} />
     </div>
   )
 }
-
-export default Home
