2a765869cbab124565e49a76f3c8544ee40d497c
Luis Quiros <lquiros@akurey.com>
2022-11-17 09:54:35 -0600

    Fix key issue friends

--- frontend/src/pages/profile/Friends.js
+++ frontend/src/pages/profile/Friends.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 export default function Friends({ friends }) {
-  console.log(friends)
   return (
     <div className='profile_card'>
       <div className='profile_card_header'>
@@ -18,10 +17,11 @@ export default function Friends({ friends }) {
       )}
       <div className='profile_card_grid'>
         {friends &&
-          friends.slice(0, 9).map((friend) => (
+          friends.slice(0, 9).map((friend, i) => (
             <Link
               to={`/profile/${friend.username}`}
               className='profile_photo_card'
+              key={i}
             >
               <img src={friend.picture} alt='' />
               <span>
